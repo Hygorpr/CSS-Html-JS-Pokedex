659125fb07552a750eeb8946c038f8016c0f3df0
Hygor <godomosto@gmail.com>
2023-09-08 18:59:35 -0300

    Pokedex

---
+++ assets/README.md
@@ -0,0 +1,19 @@
+# Pokedex
+
+Este é um projeto de Pokedex simples, criado usando HTML, CSS e JavaScript. A Pokedex é uma enciclopédia eletrônica que contém informações sobre diversos Pokémon. Neste projeto, você pode visualizar detalhes sobre diferentes Pokémon, incluindo seus nomes e tipos.
+
+## Tecnologias Utilizadas
+
+- [HTML: A estrutura básica da página web.](https://developer.mozilla.org/pt-BR/docs/Web/HTML)
+
+- [CSS: Estilização e layout da Pokedex.](https://developer.mozilla.org/pt-BR/docs/Web/CSS)
+
+- [JavaScript: Manipulação dos dados dos Pokémon e funcionalidades interativas.](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)
+
+- [API de Pokémon: Para obter informações detalhadas sobre os Pokémon, utilizamos a PokeAPI.](https://pokeapi.co/)
+
+**Contribuições
+Contribuições são bem-vindas! Se você deseja melhorar ou adicionar recursos à Pokedex, sinta-se à vontade para criar um fork deste repositório e enviar um pull request.**
+
+Autor
+Hygor Prado
---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('LoadMoreButton')
+
+
+const maxRecords = 25
+const limit = 10
+let offset = 0;
+
+
+
+
+// Converter pokemon para uma lista
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+
+// Carregar os pokemons
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+// Limite dos pokemons
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+const qtdRecordNextPage = offset + limit
+
+    if (qtdRecordNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    }   else {
+        loadPokemonItens(offset, limit)  
+    }
+
+
+
+})
